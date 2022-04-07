d1e3750d1d8b4d8611819d065ebdc98aa7a9906b
Chris-Andiego <andiegochris@gmail.com>
2022-04-07 15:40:27 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
-/*main function
+/*
+ * main function
  *
  * Shoul return value zero
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
