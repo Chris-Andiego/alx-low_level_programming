8d517fc31019b146615d2c3c55dbc0ed324bf46f
Chris-Andiego <andiegochris@gmail.com>
2022-04-08 19:52:01 -0700

    changes

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 0 is successful
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'z'; low >= 'a'; low--)
+		putchar(low);
+	
+	putchar('\n');
+	return 0;
+}
