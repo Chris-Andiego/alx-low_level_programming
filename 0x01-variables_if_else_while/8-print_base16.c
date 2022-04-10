7b5a90c5a5fffb4cb54bf74366e7311b96379708
Chris-Andiego <andiegochris@gmail.com>
2022-04-10 04:53:45 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Reference: 0 is success
+ */
+
+int main(void)
+{
+	int n;
+	char low;
+
+	for (n = '0'; n < '9'; n++)
+		putchar(n);
+	for (low = 'a'; low <= 'f'; low++)
+		putchar(low);
+	putchar('\n');
+
+	return (0);
+}
