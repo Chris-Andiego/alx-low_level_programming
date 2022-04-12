caedc37ce76c96d223bd19d227bc3d55057a91aa
Chris-Andiego <andiegochris@gmail.com>
2022-04-12 16:30:37 -0700

    letter

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,4 +1,4 @@
-#Include "main"
+#include "main"
 
 /**
  * _isalpha - check main
@@ -7,7 +7,7 @@
  * Return: 1 or 0
  */
 
-int -isalpha(int c)
+int _isalpha(int c)
 {
 	char lower, upper;
 	int isletter = 0;
