449d3242a0452d44cb671fa15ffb778e4317d9cf
Chris-Andiego <andiegochris@gmail.com>
2022-04-13 13:10:57 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @r : input
+ * Description: returns value of a number
+ * Return: absolute value or number
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
